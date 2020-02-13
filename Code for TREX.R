0d5977af33ae2b3d234ac493139aca714d1d849d
Matt Clark <matthewclark989@boisestate.edu>
2020-02-12 18:19:04 -0700

    code ready

--- Code for TREX.R
+++ Code for TREX.R
@@ -2,3 +2,13 @@ library(rstanarm)
 library(tidyverse)
 options(mc.cores = parallel::detectCores())
 dat<-read_csv("~/Smoke_Proj/Data/MergedDataComplete.csv")
+
+fit<-stan_glmer.nb(RecreationVisits~
+                    (1|CatColM)+
+                    (stdsmoke|UnitCode)+
+                     Year, 
+                  data=dat)
+
+
+
+
