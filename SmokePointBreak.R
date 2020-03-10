3200a4694cfe4e05dca2280e4de17b52cd7ec993
Matt Clark <matthewclark989@boisestate.edu>
2020-03-10 14:37:24 -0600

    AddedMod for individual park slopes

--- SmokePointBreak.R
+++ SmokePointBreak.R
@@ -1,18 +1,50 @@
 library(tidyverse)
 library(rstan)
 dat<-read_csv("~/Smoke_Proj/Data/MergedDataComplete.csv")
-dat<-dat[1:7895,]
+#dat<-dat%>% filter(UnitCode %in% c("YOSE","YELL","GRTE"))
+dat<-dat%>% filter(UnitCode %in% c("YELL","GRTE"))
+#dat$id<-as.numeric(as.factor(dat$UnitCode )) for no seasonal intercept
 dat$id<-as.numeric(as.factor(dat$UnitCode ))
+
 data_list <- list(
   N = nrow(dat),
   Nprk = length(unique(dat$UnitCode)),
   count = dat$RecreationVisits,
   smoke = dat$stdsmoke,
   pcode = dat$id)
-
-l<- stan( file="SmokePointBreak.stan" , data=data_list,chains=1 )
+options(mc.cores=3)
+l<- stan( file="IndividualPointBreakIndSlopes.stan" , data=data_list,chains=3 )
 
 print( l , probs=c( (1-0.89)/2 , 1-(1-0.89)/2 ) )
 summary(l)
 
- 
+
+
+
+
+#now do with full ds
+dat<-read_csv("~/Smoke_Proj/Data/MergedDataComplete.csv")
+
+stdize<-function(x) {return((x-mean(x)/(2*sd(x))))}
+
+dat<-dat%>%group_by(UnitCode)%>%
+  mutate(stdsmokepark=stdize(stdsmoke))
+
+dat$id<-as.numeric(as.factor(dat$UnitCode ))
+data_list <- list(
+  N = nrow(dat),
+  Nprk = length(unique(dat$UnitCode)),
+  count = dat$RecreationVisits,
+  smoke = dat$stdsmokepark,
+  pcode = dat$id)
+
+l<- stan( file="IndividualPointBreak.stan" , data=data_list,chains=3 )
+
+print( l , probs=c( (1-0.89)/2 , 1-(1-0.89)/2 ) )
+
+
+ggplot(data=dat,aes(x=stdsmoke,y=RecreationVisits,by=UnitCode))+
+  geom_point(alpha=0.2)+
+  geom_smooth(aes(color=UnitCode),se=FALSE,alpha=0.1)+
+  theme_classic()+ggtitle("max")
+
